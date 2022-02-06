bc79bb5cd391a9c14d1bd430fda9727aa3d8ce6b
kiin The Dovah <kiinthedovah@gmail.com>
2022-02-05 23:37:23 -0500

    Improved rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,9 @@ public class PlayerController : MonoBehaviour
   Vector2 mousePosition;
   Vector2 direction;
   float angle;
-  Vector3 targetRotation;
-  public float turnSpeed = 45;
-  public float moveSpeed = 25;
+  float impulse;
+  public float turnSpeed;
+  public float moveSpeed;
   Rigidbody2D rb;
   // Start is called before the first frame update
   void Start()
@@ -27,10 +27,9 @@ public class PlayerController : MonoBehaviour
     mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     // Get the direction of the mouse relative to the player and rotate the player to said direction
     direction = mousePosition - (Vector2)transform.position;
-    angle = Vector2.SignedAngle(Vector2.down, direction);
-    // transform.eulerAngles = new Vector3(0, 0, angle);
-    targetRotation = new Vector3(0, 0, angle);
-    // rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime));
+    angle = Vector2.SignedAngle(-transform.up, direction);
+    impulse = angle * Mathf.Deg2Rad * turnSpeed * rb.inertia;
+    rb.AddTorque(impulse, ForceMode2D.Force);
 
     if (Input.GetKeyDown(KeyCode.Mouse0))
     {
