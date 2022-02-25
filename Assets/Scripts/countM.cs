075b95c911449f0fdbbfd421b2aa1f7f22de3151
kiin The Dovah <kiinthedovah@gmail.com>
2022-02-25 16:26:53 -0500

    enemies are back

--- Assets/Scripts/countM.cs
+++ Assets/Scripts/countM.cs
@@ -2,36 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
- 
+
 public class countM : MonoBehaviour
 {
-    public TMP_Text multiplier;
-    int contador;
-    Rigidbody2D rb;
-   
+  public TMP_Text multiplier;
+  int contador;
+  Rigidbody2D rb;
+
 
   private void OnTriggerEnter2D(Collider2D collider)
   {
-     GameObject objeto1 = GameObject.Find("bullet");
-    if (collider.gameObject.name == "Square") 
+    GameObject objeto1 = GameObject.Find("bullet");
+    if (collider.gameObject.name == "Square")
     {
-        contador = contador + 1;
-        multiplier.text = "x" + contador;
-        Destroy(collider.gameObject);
+      contador = contador + 1;
+      multiplier.text = "x" + contador;
+      Destroy(collider.gameObject);
     }
-    else    
+    else
     {
-        contador = 0;
+      contador = 0;
     }
-    
+
   }
 
-  public void Awake()
-  {
-    rb = GetComponent<Rigidbody2D>();
-    contador = 0;
-    multiplier.text = "x" + contador;
-  }
+  // public void Awake()
+  // {
+  //   rb = GetComponent<Rigidbody2D>();
+  //   contador = 0;
+  //   multiplier.text = "x" + contador;
+  // }
 }
 
 
