e8959d02d0d9baaf45e5a4b82ce61cb435c72b0f
kiin The Dovah <kiinthedovah@gmail.com>
2022-02-25 18:12:39 -0500

    temp

--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -1,24 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class WallController : MonoBehaviour
 {
-  int counter;
-  string text = "";
-  public TextMeshProUGUI multiplier;
-  public Text puntos;
-  private void OnTriggerEnter2D(Collider2D other)
+  //public TMP_Text multiplier;
+  private void OnTriggerEnter2D(Collider2D col)
   {
-    //counter = int.Parse(
-    Debug.Log(multiplier);
-    //text = multiplier.text;
-    //counter++;
-    //text = counter.ToString();
-    //multiplier.text = text;
-    //puntos.text = int.Parse(puntos.text + counter).ToString();
-    Destroy(other.gameObject);
+    if (col.tag == "bullet")
+    {
+      //multiplier.text = "X0";
+      Destroy(col.gameObject);
+    }
   }
 }
