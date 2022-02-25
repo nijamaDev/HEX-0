68220d6c63c3e8863ce25dc354ff17f2e506f7b2
Van <45314803+vantwot@users.noreply.github.com>
2022-02-24 22:02:43 -0500

    Enemy Meter

---
+++ Assets/Scripts/count.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class count : MonoBehaviour {
+
+ int contador;
+ public Text puntos;
+ Rigidbody2D rb;
+
+ void Start () {
+
+ }
+
+private void OnTriggerEnter2D(Collider2D other)
+ {
+contador = contador + 1;
+ puntos.text = "" + contador;
+ Destroy(other.gameObject);
+
+}
+
+public void Awake()
+ {
+
+ rb = GetComponent<Rigidbody2D>();
+ contador = 0;
+ puntos.text = "" + contador;
+
+
+ }
+
+}
